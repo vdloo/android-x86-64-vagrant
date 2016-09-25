8f8fb6fedcc525aed9e92d3883d5b5dbc2eaea8b
Rick van de Loo <rickvandeloo@gmail.com>
2016-09-25 14:06:08 +0200

    fix pacman after provisioning

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -8,3 +8,11 @@ if ssh-add -L >/dev/null 2>/dev/null; then
     agent_keys=$(ssh-add -L | awk '!NF || !seen[$0]++' "$auth_keys_file" -)
     echo "$agent_keys" > "$auth_keys_file"
 fi
+
+# Fix pacman by force-reinstalling python, sometimes it is b0rked after packaging
+rm -f /var/lib/pacman/db.lck
+pacman -Rsc python --noconfirm 2> /dev/null
+pacman -S --force python --noconfirm 2> /dev/null
+
+# Install additional tools
+pacman -S tar procps --noconfirm
